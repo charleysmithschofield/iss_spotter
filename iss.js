a39ea95c8a15ed4a1230e1020c7bf5894b9bb65a
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-24 21:42:35 -0700

    add fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -9,7 +9,7 @@
 // Require the request module
 const request = require('request');
 
-// Defin fetchMyIp function that takes in a callback as its argument
+// Define fetchMyIp function that takes in a callback as its argument
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request('https://api.ipify.org/?format=json', (error, response, body) => {
@@ -24,11 +24,42 @@ const fetchMyIP = function(callback) {
       return;
     }
 
-
-    // If everything is successful, extract the IP sddress from the response body and pass it to the callback
+    // If everything is successful, extract the IP address from the response body and pass it to the callback
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
 };
 
-module.exports = { fetchMyIP };
+// Define the fetchCoordsByIP
+const fetchCoordsByIP = function(ip, callback) {
+  // use request to fetch IP address from
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    // if there was an error, return the error and a null IP address
+    if (error) return callback(error, null);
+      
+    // if the statusCode is not equal to 200 (aka OK)
+    if (response.statusCode !== 200) {
+      
+      // Create a new Error object with a message containing the statusCode, and pass it to the callback with null.
+      callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+      return;
+    }
+
+    // Parse the response body
+    const parsedBody = JSON.parse(body);
+
+    // Check if anything failed
+    if (!parsedBody.success) {
+      // Initialize a variable for a message to use if anything failed
+      const msg = `Failed to get geographical location for IP. Error:, ${parsedBody.message}`;
+      // If anything failed, return a new Error object with a message and pass it to the callback with null.
+      return callback(Error(msg), null);
+    }
+    // Include the latitude and longitude in the API response
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
